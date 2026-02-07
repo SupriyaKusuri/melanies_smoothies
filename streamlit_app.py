d8da9764c9dd2f1cffec400ea3e6998c8a3d52bf
SupriyaKusuri <supriya.yadav@gmail.com>
2026-02-07 22:26:42 +0000

    Update streamlit_app.py
    added response under for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 ingredients_list = st.multiselect(
     'Choose upto 5 fruits:'
     , my_dataframe
@@ -32,6 +29,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         #ingredients_string += "'"+ fruit_chosen + "', "
         ingredients_string += fruit_chosen + " "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order +"')"""
